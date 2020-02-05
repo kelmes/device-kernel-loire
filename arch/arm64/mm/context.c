758703f56ecd60d41170919befbc4585f044768e
Alfred Neumayer <dev.beidl@gmail.com>
2020-02-05 14:18:16 +0100

    treewide: Linux 4.4.197

--- arch/arm64/mm/context.c
+++ arch/arm64/mm/context.c
@@ -204,7 +204,8 @@ asmlinkage void post_ttbr_update_workaround(void)
 
 static int asids_init(void)
 {
-	int fld = cpuid_feature_extract_field(read_cpuid(SYS_ID_AA64MMFR0_EL1), 4);
+	int fld = cpuid_feature_extract_field(read_cpuid(SYS_ID_AA64MMFR0_EL1), 4,
+										  false);
 
 	switch (fld) {
 	default:
